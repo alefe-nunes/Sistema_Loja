1d08264626e80f30a0179716620924cdcf5da495
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-12 14:03:57 -0300

    Iniciar projeto

---
+++ src/main/java/br/com/zup/Sistema_Loja/SistemaLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Sistema_Loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Sistema_Loja/SistemaLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Sistema_Loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
