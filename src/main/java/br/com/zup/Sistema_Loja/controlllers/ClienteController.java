7f286ee679a209e6b5373b531943fd9694a4ab48
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-12 18:31:06 -0300

    Adicionar Service e Controller do cliente

--- src/main/java/br/com/zup/Sistema_Loja/controlllers/ClienteController.java
+++ src/main/java/br/com/zup/Sistema_Loja/controlllers/ClienteController.java
@@ -1,19 +1,38 @@
 package br.com.zup.Sistema_Loja.controlllers;
 
+import br.com.zup.Sistema_Loja.dtos.ClienteDTO;
 import br.com.zup.Sistema_Loja.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping
 @RestController
 public class ClienteController {
 
     @Autowired
-    private ClienteService cliente;
+    private ClienteService clienteService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarCliente(@RequestBody ClienteDTO cliente) {
+        clienteService.cadastrarCliente(cliente);
+    }
 
 
+    @GetMapping
+    public List<ClienteDTO> exibirListaDeClientes() {
+        return clienteService.exibirListaDeClientes();
 
+    }
+
+    @GetMapping("/{cpf}")
+    public ClienteDTO pesquisarClientePeloCPF(@PathVariable String cpf) {
+        return clienteService.pesquisarClientePeloCPF(cpf);
+
+    }
 
 
 }
--- src/main/java/br/com/zup/Sistema_Loja/services/ClienteService.java
+++ src/main/java/br/com/zup/Sistema_Loja/services/ClienteService.java
@@ -1,7 +1,42 @@
 package br.com.zup.Sistema_Loja.services;
 
+import br.com.zup.Sistema_Loja.dtos.ClienteDTO;
+import br.com.zup.Sistema_Loja.dtos.ProdutoDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ClienteService {
+
+    private List<ClienteDTO> listaDeClientes = new ArrayList<>();
+
+    public void cadastrarCliente(ClienteDTO cliente) {
+
+        listaDeClientes.add(cliente);
+
+    }
+
+
+    public ClienteDTO pesquisarClientePeloCPF(String cpf) {
+
+        for (ClienteDTO cliente : listaDeClientes) {
+            if (cliente.getCpf().equals(cpf)) {
+                return cliente;
+            }
+
+        }
+
+        throw new RuntimeException("Cliente não encontrado. CPF inválido");
+
+
+    }
+
+
+    public List<ClienteDTO> exibirListaDeClientes() {
+        return this.listaDeClientes;
+
+    }
+
 }
