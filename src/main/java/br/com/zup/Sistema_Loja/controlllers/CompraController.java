64d20b2e3a25b31dd49170b4e79e69d5652ed9f5
alefe.nunes <alefe.nunes@zup.com.br>
2021-08-13 01:23:47 -0300

    Implementar Compra Service e Controller

--- src/main/java/br/com/zup/Sistema_Loja/controlllers/CompraController.java
+++ src/main/java/br/com/zup/Sistema_Loja/controlllers/CompraController.java
@@ -1,14 +1,30 @@
 package br.com.zup.Sistema_Loja.controlllers;
 
+import br.com.zup.Sistema_Loja.dtos.CompraDTO;
 import br.com.zup.Sistema_Loja.services.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+import java.util.List;
+
+
 @RestController
+@RequestMapping ("/compras")
 public class CompraController {
 
     @Autowired
     private CompraService compraService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarCompra(@RequestBody CompraDTO compra) {
+        compraService.cadastrarCompra(compra);
+    }
+
+    @GetMapping
+    public List <CompraDTO> exibirListaDeCompras () {
+        return compraService.exibirListaDeCompras();
+    }
+
 }
--- src/main/java/br/com/zup/Sistema_Loja/services/CompraService.java
+++ src/main/java/br/com/zup/Sistema_Loja/services/CompraService.java
@@ -1,7 +1,26 @@
 package br.com.zup.Sistema_Loja.services;
 
+import br.com.zup.Sistema_Loja.dtos.CompraDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CompraService {
+
+    private List<CompraDTO> listaDeCompras = new ArrayList<>();
+
+    public CompraDTO cadastrarCompra(CompraDTO compra) {
+        listaDeCompras.add(compra);
+        return compra;
+    }
+
+
+    public List<CompraDTO> exibirListaDeCompras() {
+        return this.listaDeCompras;
+
+    }
+
+
 }
